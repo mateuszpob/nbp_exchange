a9bffeda97531093d8942b5d13e869cdc91481c1
mp <mateuszpob@gmail.com>
2023-02-13 12:27:26 +0100

    view + controller + route added

---
+++ app/Http/Controllers/NbpController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CurrencyService;
+
+class NbpController extends Controller
+{
+    public function updateExchanges()
+    {
+
+        return 'OK';
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NbpController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/update-exchange', [NbpController::class, 'updateExchanges'])->name('update.exchange');
